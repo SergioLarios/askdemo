33755f728372f4f01f7bf7e87cb921a3e1f38d9e
Sergio Larios <sergiolarios97@gmail.com>
2018-04-19 20:52:06 +0200

    Creada conexión con Hybris

--- apps/vass-test/funcs.js
+++ apps/vass-test/funcs.js
@@ -1,36 +1,49 @@
 var levenshtein = require('fast-levenshtein');
+var request = require('request');
 
 /**
  * Private constants
  */
-
- const session_order = 'order';
- const TEMP_menu = [
-	 'coffee',
-	 'coffee with milk',
-	 'stake',
-	 'salad',
-	 'water'
- ];
+const session_order = 'order';
+var menu = [];
+var userId = 'alexa_client';
+var itemCode = '3965240';
+var securityCode = '123';
+var token, cartId, code, addresses, deliveryModeId, paymentDetailsId;
 
 /**
  * Class that contains all the alexa events
  */
 class AlexaFuncs {
-
 	/**
 	 * Public methods
 	 */
 
 	static launch(req, res) {
 
+		AlexaFuncs._getToken();
+		AlexaFuncs._getProducts(); // TODO: añadir productos a menu, debe guardarse el nombre y el código para poder añadirlo al carrito
 		res.say('Hi, welcome to VASS test. Would you like to start a new order or listen the menu?');
 		res.shouldEndSession(false);
 
 	}
-	 
+
 	static newOrder(req, res) {
 
+		// Create new cart
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts',
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			cartId = json.code;
+			console.log("Cart Id (code): ", cartId);
+
+		});
 		res.say('You have started a new order, what would you like to add?');
 		AlexaFuncs._initOrder(req);
 		res.shouldEndSession(false);
@@ -38,31 +51,55 @@ class AlexaFuncs {
 	}
 
 	static addItem(req, res) {
-		
+
 		if (AlexaFuncs._hasOrder(req)) {
 			const item = req.slot('item');
-			const quantity = req.slot('quantity');
-			const orderItem = { item: item, quantity: quantity, accepted: false};
-			AlexaFuncs._pushToOrder(req, orderItem);
-			res.say(`${quantity} ${item} added, is it correct?`);
+			const quantity = req.slot('quantity') || 1;
 
-			TEMP_menu.forEach(el => {
-				console.log(item, el, levenshtein.get(item, el));
+
+			let scores = [];
+			menu.forEach(el => {
+				const elms = el.name.split(" ");
+				const itemElms = item.split(" ");
+				let distance = 0;
+				elms.forEach(elemnt => {
+					itemElms.forEach(item => {
+						let ld = levenshtein.get(elemnt, item);
+						if (ld < distance || distance == 0) {
+							distance = ld;
+						}
+					});
+				});
+				const score = [el, distance];
+				scores.push(score);
 			});
+
+			const sortedScores = scores.sort(AlexaFuncs.sortfunc);
+			console.log(sortedScores);
+
+			const orderItem = { item: sortedScores[0][0].code, quantity: quantity, accepted: false };
+			AlexaFuncs._pushToOrder(req, orderItem);
+			res.say(`${quantity} ${sortedScores[0][0].name} added, is it correct?`);
+
 		}
 		else {
 			res.say(`No order found`);
-		}	
+		}
 		res.shouldEndSession(false);
+	}
+
+	static sortfunc(a, b) {
+		return a[1] - b[1];
 	}
 
 	static menu(req, res) {
 
+		// Send menu to alexa
 		let result = 'The current menu has:';
-		TEMP_menu.forEach(item => {
-			result = `${result} ${item}, `;
+		menu.forEach(item => {
+			result = `${result} ${item.name}, `;
 		});
-		result = result.slice(0,-2);
+		result = result.slice(0, -2);
 
 		res.say(result);
 		res.shouldEndSession(false);
@@ -76,7 +113,11 @@ class AlexaFuncs {
 				res.say('Your order is empty.');
 			} else {
 				res.say('Your order will be processed.');
+
+				// Start the process in hybris
+				AlexaFuncs._getUserAddresses();
 			}
+
 			AlexaFuncs._emptyOrder(req);
 		} else {
 			res.say('No order found.');
@@ -86,7 +127,7 @@ class AlexaFuncs {
 	}
 
 	static listOrder(req, res) {
-	
+
 		if (AlexaFuncs._hasOrder(req)) {
 			if (AlexaFuncs._orderIsEmpty(req, true)) {
 				res.say('Your order is empty.');
@@ -96,9 +137,9 @@ class AlexaFuncs {
 				order.forEach(item => {
 					if (item.accepted) {
 						result = `${result} ${item.quantity} ${item.item}, `;
-					}			
+					}
 				});
-				result = result.slice(0,-2);
+				result = result.slice(0, -2);
 				res.say(result);
 			}
 		} else {
@@ -113,6 +154,7 @@ class AlexaFuncs {
 		res.say('Canceling order');
 		AlexaFuncs._emptyOrder(req);
 		res.shouldEndSession(false);
+		AlexaFuncs._cancelCart();
 
 	}
 
@@ -120,12 +162,12 @@ class AlexaFuncs {
 
 		if (AlexaFuncs._hasOrder(req)) {
 			res.say('You are currently in and order, try to add items like: add one coffee. ' +
-			'If you want to check your current order try saying: List order. Also you can ' +
-			'cancel the current order, finish it or list the menu');
+				'If you want to check your current order try saying: List order. Also you can ' +
+				'cancel the current order, finish it or list the menu');
 		}
 		else {
 			res.say('No order found. You can start a new order by saying: new order. Also ' +
-			'you can list the menu or close the skill by saying: stop');
+				'you can list the menu or close the skill by saying: stop');
 		}
 		res.shouldEndSession(false);
 
@@ -147,8 +189,25 @@ class AlexaFuncs {
 				order[order.length - 1].accepted = true;
 				AlexaFuncs._saveOrder(req, order);
 				res.say('Item added.');
+				// TODO: Aquí debería añadir el item elegido y no el item con codigo "hardcoded"
+				request({
+					url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId + '/entries',
+					method: 'POST',
+					headers: {
+						'Accept': 'application/json',
+						'Authorization': 'Bearer ' + token,
+						'Content-Type': 'application/x-www-form-urlencoded'
+					},
+					form: {
+						'code': order[order.length - 1].item
+					}
+				}, function (err, res) {
+					var json = JSON.parse(res.body);
+					console.log("Item added to Hybris cart:", json);
+				});
+
 			}
-			
+
 		}
 		else {
 			res.say('No order found.');
@@ -165,11 +224,11 @@ class AlexaFuncs {
 			}
 			else {
 				let order = AlexaFuncs._getOrder(req);
-				order.splice(-1,1);
+				order.splice(-1, 1);
 				AlexaFuncs._saveOrder(req, order);
 				res.say('Item removed.');
 			}
-			
+
 		}
 		else {
 			res.say('No order found.');
@@ -217,6 +276,188 @@ class AlexaFuncs {
 		AlexaFuncs._saveOrder(req, order);
 	}
 
+	// Get token
+	static _getToken() {
+		request({
+			url: 'https://188.79.1.60:9002/authorizationserver/oauth/token',
+			method: 'POST',
+			form: {
+				'client_id': 'ALEXA_CLIENT',
+				'client_secret': 'ALEXA_CLIENT',
+				'username': 'ALEXA_APP',
+				'password': 'ALEXA_APP',
+				'grant_type': 'client_credentials'
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			token = json.access_token;
+			console.log("Access Token:", token);
+		});
+	}
+
+	// Get Products
+	static _getProducts() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/products/search?query=:BASIC:category:597&pageSize=10&fields=products(code,name,price),pagination',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			json.products.forEach((el) => {
+				var product = {};
+				product.code = el.code;
+				product.name = el.name;
+				menu.push(product);
+			});
+			console.log("Menu:", menu);
+		});
+	}
+
+	// Get user addresses
+	static _getUserAddresses() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/addresses',
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token,
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			console.log("Addresses:", json); // TODO: recoger addressId
+			AlexaFuncs._setCartUserAddresses();
+		});
+	}
+
+	// Definir addresses al cart
+	static _setCartUserAddresses() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId + '/addresses/delivery',
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			form: {
+				'addressId': '8796125822999', // TODO: poner address de _getUserAddresses
+			}
+		}, function (err, res) {
+			if (!err) {
+				AlexaFuncs._getCartDeliveryModes();
+			}
+		});
+	}
+
+	// Get user delivery modes
+	static _getCartDeliveryModes() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId + '/deliverymodes',
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			console.log("Delivery modes:", json);
+			AlexaFuncs._setCartDeliveryMode();
+		});
+	}
+
+	// Definir user delivery mode al cart
+	static _setCartDeliveryMode() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId + '/deliverymode',
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			form: {
+				'deliveryModeId': 'standard-gross', // TODO: poner deliveryModeId de _getCartDeliveryModes
+			}
+		}, function (err, res) {
+			if (!err) {
+				AlexaFuncs._getUserPaymentMethods();
+			}
+		});
+	}
+
+	// Get user payment methods
+	static _getUserPaymentMethods() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/paymentdetails?saved=false',
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			console.log("Payment Methods:", json);
+			AlexaFuncs._setCartPaymentMethods();
+		});
+	}
+
+	// Definir payment methods al cart
+	static _setCartPaymentMethods() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId + '/paymentdetails',
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			form: {
+				'paymentDetailsId': '8796093186090', // TODO: poner paymentDetailsId de _getCartDeliveryModes
+			}
+		}, function (err, res) {
+			if (!err) {
+				AlexaFuncs._cartCheckout();
+			}
+		});
+	}
+
+	// Checkout cart
+	static _cartCheckout() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/orders',
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			form: {
+				'cartId': cartId,
+				'securityCode': securityCode // TODO: quitar securityCode hardcoded
+			}
+		}, function (err, res) {
+			var json = JSON.parse(res.body);
+			console.log("Checkout:", json);
+		});
+	}
+
+	// Cancell cart
+	static _cancelCart() {
+		request({
+			url: 'https://188.79.1.60:9002/rest/v2/electronics/users/' + userId + '/carts/' + cartId,
+			method: 'DELETE',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token
+			}
+		}, function (err, res) {
+			if (!err) {
+				console.log("Cart Cancelled");
+			}
+		});
+	}
 }
 
 module.exports = AlexaFuncs;
